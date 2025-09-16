9a8f67cd32309166431dfc42c2444bf897834abb
Kuba Bayo <bayos24@spstrutnov.cz>
2025-09-16 11:18:30 +0200

    Konec Smitec

--- Hry/Hry/Program.cs
+++ Hry/Hry/Program.cs
@@ -1,11 +1,1 @@
-﻿namespace Hry
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
-nhbfghgfdijhgn bdjhgnhjdgdgsgsgdsghdsh
+﻿
