a3a7261e3c8f196d0b75f62d7bb23e59ea5d6b80
Kuba Bayo <bayos24@spstrutnov.cz>
2025-09-16 11:09:11 +0200

    1
    10

---
+++ Hry/Hry/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Hry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+nhbfghgfdijhgn bdjhgnhjdgdgsgsgdsghdsh
